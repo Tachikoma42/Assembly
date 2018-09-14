76df287662e01769172474a00253bd0cf82d4242
Tachikoma42 <xkzmx1@gmail.com>
2018-09-14 23:24:26 +0800

    updateForExp

--- inputadd.asm
+++ inputadd.asm
@@ -8,6 +8,17 @@ CODE    SEGMENT                     ;定义代码段
 START:  MOV     AX,DATA             ;初始化DS
         MOV     DS,AX               ;初始化DS              
         ;填写代码
+        MOV     AH,1
+        INT     21H
+        MOV     BL,AL
+        MOV     AH,1
+        INT     21H
+        ADD     AL,BL
+        AAA
+        MOV     DL,AL
+        ADD     DL,30H
+        MOV     AH,2
+        INT     21H
         MOV     AX,4C00H            ;返回DOS
         INT     21H;
 CODE    ENDS                        ;代码段结束
